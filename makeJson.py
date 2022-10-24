3f1a56dad32cec157a9a7014b7c4f389fcd1aa52
Mike Munns <Michael.Munns@grassvalley.com>
2022-10-24 17:02:51 +0100

    JSON file maker, folder structure

---
+++ makeJson.py
@@ -0,0 +1,142 @@
+import json
+import tkinter as tk
+import tkinter.filedialog as fd
+from functions import yesNo
+
+root = tk.Tk()
+root.withdraw()
+
+# setDriverVersion
+def setDriverVersion(inputFile):
+    with open(inputFile, 'r') as file:
+        lines = file.readlines()
+        search = 'StaticString'
+        
+        matchLines = []
+        i = 0
+        
+        # find lines matching search, add to list
+        for line in lines:
+            if search in line:
+                matchLines.insert(i, line)
+                i += 1
+        
+        file.close()
+        
+    # if list is empty, set driverVersion=0.0.1 indicating no driver version detected
+    if len(matchLines)==0:
+        driverVersion='0.0.1'
+        
+    # set driverVersion to match SVN check-in number
+    elif len(matchLines)==1:
+        # read in string
+        s0=''.join(matchLines[0])
+        # split at '.rsnmp '
+        s1=s0.split('.rsnmp ')
+        # split at ' '
+        s2=''.join(s1[1])
+        s3=s2.split(' ')
+        # grab the svn version number
+        svnVersion=s3[0]
+        # set the driver version number
+        driverVersion=svnVersion+'.0.0'
+        
+    # catch multiple entries, set driverVersion=0.0.9 to indicate multiple candidates for driver version
+    else:
+        driverVersion='0.0.9'
+        
+    return driverVersion
+
+# setIncludes
+#need to find dependant drivers
+def setIncludes(inputFile):
+    l0=inputFile.split('\\')
+    # handle dependant drivers
+    dependCheck = yesNo('Are there any dependant RollSNMP drivers?', default='no')
+    if dependCheck==True:
+        l1=fd.askopenfilenames(title='Select files...')
+        l2 = list(l1)
+        l3=[]
+        for a in l2:
+            b=a.split('/')
+            l3.append(b[-1])
+        l3.append(l0[-1])
+        includes = ', '.join(l3)
+    
+    else:
+        includes = l0[-1]
+    
+    return includes
+
+# setManufacturer
+def setManufacturer(inputFile):
+    l0=inputFile.split('\\')
+    
+    # get XML position
+    if 'XML' in l0: xmlPos=l0.index('XML')
+    elif 'xml' in l0: xmlPos=l0.index('xml')
+    else: return 'Manufacturer error - no XML in path.'
+    
+    # move to manufacturer position and return manufacturer
+    manufacturer = l0[xmlPos-2]
+    return manufacturer
+
+# setDriverName
+def setDriverName(inputFile):
+    with open(inputFile, 'r') as file:
+        lines = file.readlines()
+        search = '<UnitTypeDef'
+        
+        matchLines = []
+        i = 0
+        
+        # find lines matching search, add to list
+        for line in lines:
+            if search in line:
+                matchLines.insert(i, line)
+                i += 1
+            
+        file.close()
+            
+        flag=False
+        # if list is empty or greater than 1, set driverName to file path
+        if len(matchLines)==0: flag=True
+        
+        elif len(matchLines)>1: flag=True
+        
+        if flag==True:
+            l0=inputFile.split('\\')
+            
+            # get XML position
+            if 'XML' in l0: xmlPos=l0.index('XML')
+            elif 'xml' in l0: xmlPos=l0.index('xml')
+            else: return 'Driver Name error - no XML in path.'
+            
+            # move to manufacturer position and return manufacturer
+            driverName = l0[xmlPos-1]
+            
+        # set driverName to match <UnitTypeDef> entry
+        else:
+            # read in string
+            s0=''.join(matchLines[0])
+            # split at 'typeName='
+            s1=s0.split('typeName=')
+            # split at 'snmpVersion'
+            s2=''.join(s1[1])
+            s3=s2.split('\"')
+            # remove quotes and return driverName
+            driverName=s3[1].strip('\"')
+            
+        return driverName
+
+# build json
+def jsonData(inputFile):
+    includes=setIncludes(inputFile)
+    manufacturer=setManufacturer(inputFile)
+    driverName=setDriverName(inputFile)
+    driverVersion=setDriverVersion(inputFile)
+    dict = {'includes':[f'{includes}'], 'manufacturer':manufacturer, 'driverName':driverName, 'driverVersion':driverVersion, 'deviceName':f'{manufacturer} - {driverName}'}
+    jsonString=json.dumps(dict, indent=4)
+    f = open('driver.json', 'w')
+    f.write(jsonString)
+    f.close
--- snmp-packager.py
+++ snmp-packager.py
@@ -1,13 +1,48 @@
-#!/usr/bin/env python3.10.5
+import os
+import tkinter as tk
+import tkinter.filedialog as fd
+from makeJson import jsonData
 
-# ---- TO DO ----
+root = tk.Tk()
+root.withdraw()
+
+# create folder structure
+def folderStructure(path):
+    try:
+        fullpath = path+'\GVO\MIBs'
+        print(fullpath)
+        os.mkdir(fullpath)
+        #OS ERROR HERE
+    except OSError:
+        print ("Creation of the directory %s failed" % fullpath)
+    else:
+        print ("Successfully created the directory %s " % fullpath)
+
+
+# select inputFile
+def inputFile():
+    types=[('RollSNMP(*.rsnmp)','*.rsnmp')]
+    fullFile = fd.askopenfilename(title='Select files...', filetypes=types)
+    if fullFile == '': inputFile=False
+    else:
+        parts = fullFile.split('/')
+        inputFile={'fileName':parts[-1],'filePath':'\\'.join(parts[:-1])}
+    return inputFile
+    
 
 # create JSON file
 
-# read JSON file
 
 # collect required MIBs
+
 
 # convert *.rsnmp to GVO version
 
 # zip up into package
+
+# main
+if __name__ == "__main__":
+    driverFile = inputFile()
+    folderStructure(driverFile['filePath'])
+    #THIS WORKS
+    #jsonData(driverFile['filePath']+'\\'+driverFile['fileName'])
